d9ce65da33a7b80b7666e84b0d666252c2130ac0
Piotr Fic <piotr@localhost.localdomain>
2019-10-21 23:12:18 +0200

    Dla najlepszych uczniów

---
+++ liczba_komputerow_a_sredni_wynik/liczba_komputerow_wyniki_geeks.r
@@ -0,0 +1,81 @@
+#Skrypt analizy wplywu dostepnosci komputerow na wyniki najlepszych uczniow
+
+library(haven)
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+
+#Wczytanie glownej ramki danych
+dane <- haven::read_sas("./cy6_ms_cmb_stu_qqq.sas7bdat")
+
+#Wybor kolumn: pytanie o liczbe komputerow oraz kolumny z wynikami
+df <- select(dane, ST012Q06NA, PV1MATH:PV10SCIE) %>%
+  filter(., !is.na(ST012Q06NA))
+
+#Wyodrebnienie grup najlepszych 5%
+df_math <- best_in_math(10, df)
+df_read <- best_in_reading(10, df)
+df_scie <- best_in_science(10, df)
+
+#Oddzielnie dla kazdego przedmiotu liczymy srednie wyniki
+#w grupach osob w zaleznosci od liczby komputerow w domu
+means_math <- group_by(df_math, ST012Q06NA) %>%
+  summarise(mean_1 = mean(PV1MATH),
+            mean_2 = mean(PV2MATH),
+            mean_3 = mean(PV3MATH),
+            mean_4 = mean(PV4MATH),
+            mean_5 = mean(PV5MATH),
+            mean_6 = mean(PV6MATH),
+            mean_7 = mean(PV7MATH),
+            mean_8 = mean(PV8MATH),
+            mean_9 = mean(PV9MATH),
+            mean_10 = mean(PV10MATH))
+
+means_math <- rowwise(means_math[, -1]) %>%
+  mutate(Mean = mean(mean_1:mean_10)) %>%
+  select(Mean)
+
+means_read <- group_by(df_read, ST012Q06NA) %>%
+  summarise(mean_1 = mean(PV1READ),
+            mean_2 = mean(PV2READ),
+            mean_3 = mean(PV3READ),
+            mean_4 = mean(PV4READ),
+            mean_5 = mean(PV5READ),
+            mean_6 = mean(PV6READ),
+            mean_7 = mean(PV7READ),
+            mean_8 = mean(PV8READ),
+            mean_9 = mean(PV9READ),
+            mean_10 = mean(PV10READ))
+
+means_read <- rowwise(means_read[, -1]) %>%
+  mutate(Mean = mean(mean_1:mean_10)) %>%
+  select(Mean)
+
+
+means_scie <- group_by(df_scie, ST012Q06NA) %>%
+  summarise(mean_1 = mean(PV1SCIE),
+            mean_2 = mean(PV2SCIE),
+            mean_3 = mean(PV3SCIE),
+            mean_4 = mean(PV4SCIE),
+            mean_5 = mean(PV5SCIE),
+            mean_6 = mean(PV6SCIE),
+            mean_7 = mean(PV7SCIE),
+            mean_8 = mean(PV8SCIE),
+            mean_9 = mean(PV9SCIE),
+            mean_10 = mean(PV10SCIE))
+
+means_scie <- rowwise(means_scie[, -1]) %>%
+  mutate(Mean = mean(mean_1:mean_10)) %>%
+  select(Mean)
+
+result <- cbind(c("0","1","2","3+"), means_math, means_read, means_scie)
+colnames(result) <- c("Computer_number","Mean_math", "Mean_reading", "Mean_science")
+
+to_plot <- melt(result, id.vars = "Computer_number")
+
+ggplot(to_plot, aes(x=Computer_number, y=value)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~ variable) +
+  ylab("Średni wynik") +
+  ggtitle("Liczba komputerów w domu a wyniki najlepszych uczniów") +
+  xlab("Liczba komputerów w domu")
