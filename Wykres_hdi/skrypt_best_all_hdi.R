8ba2fb3be0e7a91316903d226e88154d4871fc76
JAn <janborowka7@gmail.com>
2019-10-22 22:19:13 +0200

    next

--- Wykres_hdi/skrypt_best_all_hdi.R
+++ Wykres_hdi/skrypt_best_all_hdi.R
@@ -1,5 +1,5 @@
 # wczytywywanie danych 
-hdi <- read.csv2("/home/samba/borowskij/PPPD/TWD_01/Wykres_hdi/data.csv",sep = ",",header = FALSE)
+hdi <- read.csv("data.csv",sep = ",",header = FALSE)
 d <-  as.matrix(hdi[2,])
 colnames(hdi) <- d
 hdi <-  hdi[c(-1,-2),]
@@ -10,12 +10,26 @@ for (i in 3:30){
 hdi <- hdi[,c(1,2,28)]
 # wczytywanie kodów krajów 
 install.packages("countrycode")
-library(countrycode)
+
 cod <- codelist
 colnames(cod)
-cod_final<- cod[,c(5,36)]
-
+cod_final<- cod[,c(2,5,36)]
+## połączenie kodu krajów z danymi hdi 
 data_hdi <- merge(cod_final,hdi,by.x="country.name.en",by.y ="Country" )
-colnames(data_hdi)[2] <- "CNT"
+colnames(data_hdi)[3] <- "CNT"
+library(dplyr)
+## wprowadzanie i poprawki ramik z danymi pissa 
+dane <- haven::read_sas("../cy6_ms_cmb_stu_qqq.sas7bdat")
+dane <- select(dane, CNT,CNTSTUID,PV1MATH:PV10SCIE)
+dane <- best_in_all(10,dane)
+dane <- dane[,c(1,33)]
 
-dane <- haven::read_sas("./cy6_ms_cmb_stu_qqq.sas7bdat")
+dane_plot <-  merge(dane,data_hdi,by = "CNT")
+
+data_plot1<- dane_plot %>% group_by(CNT,country.name.en,continent,`HDI Rank (2017)`,`2015`)%>% summarise(mean_all_by_CNT=mean(average_pv_all))
+colnames(data_plot1)[5] <- "HDI"
+library(ggplot2)
+
+ggplot(data_plot1,aes(x=HDI,y=mean_all_by_CNT,colour=continent))+geom_point()+stat_smooth(method = "gam",inherit.aes = FALSE,aes(x=HDI,y=mean_all_by_CNT))
+ggsave("Wykres_hdi/best_allto_hdi.png",width = 18, height = 9)
+
