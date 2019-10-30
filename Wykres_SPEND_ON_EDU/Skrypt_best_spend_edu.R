cec41c2633354760d816c5644acc193ff35a97a0
JAn <janborowka7@gmail.com>
2019-10-30 20:13:46 +0100

    spend on edu

--- Wykres_SPEND_ON_EDU/Skrypt_best_spend_edu.R
+++ Wykres_SPEND_ON_EDU/Skrypt_best_spend_edu.R
@@ -5,12 +5,19 @@ EDU <- read.csv("EDU.csv")
 
 EDU_2015 <- EDU[ifelse(EDU[,6]==2015,TRUE,FALSE) & ifelse(EDU[,3]=="EARLYCHILDEDU",TRUE,FALSE),c(1,7)]
 ## wprowadzanie i poprawki ramik z danymi pissa 
-dane <- haven::read_sas("../cy6_ms_cmb_stu_qqq.sas7bdat")
-dane <- select(dane, CNT,CNTSTUID,PV1MATH:PV10SCIE)
-dane <- best_in_all(10,dane)## ZMIEN W CELU ZMIANY DANYCH z pissa
-dane <- dane[,c(1,33)]
+library(intsvy)
+#dane <- haven::read_sas("../cy6_ms_cmb_stu_qqq.sas7bdat")
 
-# dane o krajach 
+dane1 <- pisa2015.mean.pv(pvlabel = c("MATH","SCIE","READ"), by = "CNT", data = dane)
+
+
+#dane <- haven::read_sas("../cy6_ms_cmb_stu_qqq.sas7bdat")
+#dane <- select(dane, CNT,CNTSTUID,PV1MATH:PV10SCIE)
+#dane <- best_in_all(10,dane)## ZMIEN W CELU ZMIANY DANYCH z pissa
+dane1 <- dane1[,c(1,3)]
+  
+# dane o krajach
+library(countrycode)
 cod <- codelist
 cod_final<- cod[,c(2,5,36)]
 EDU_2015 <- na.omit(EDU_2015)
@@ -18,16 +25,27 @@ EDU_2015<- merge(EDU_2015,cod_final,by.x ="LOCATION",by.y="iso3c")
 
 
 ## łaczenie z danymi z pissa 
-dane_plot3 <- merge(dane,EDU_2015,by.x="CNT",by.y="LOCATION")
-colnames(dane_plot3)[3] <- "SPEND_EDU"
+dane_plot3 <- merge(dane1,EDU_2015,by.x="CNT",by.y="LOCATION")
+colnames(dane_plot3)[c(2,3)] <- c("mean_all_by_CNT","SPEND_EDU")
 ## grupowanie po krajach 
 library(dplyr)
-data_plot3<- dane_plot3 %>% group_by(CNT,SPEND_EDU,continent,country.name.en) %>% summarise(mean_all_by_CNT=mean(average_pv_all))
+
 
 ## Wykres
 library(ggplot2)
-ggplot(data_plot3,aes(x=SPEND_EDU,y=mean_all_by_CNT,colour=continent))+geom_point(size=4)+stat_smooth(method = "auto",inherit.aes = FALSE,aes(x=SPEND_EDU,y=mean_all_by_CNT))+
-  ggtitle("Średnia od wydatków na edukacje")+labs(y = "Średni wynik w kraju dla najlepszych 10%", x="Wydatki na eduakcje USD/Student",color="Kontynent")+theme(axis.title.x = element_text(size = 14),
-                                                                                                                                           axis.title.y = element_text(size = 14))
+library(ggrepel)
+ggplot(dane_plot3,aes(x=SPEND_EDU,y=mean_all_by_CNT,color=continent))+geom_point(size=4)+stat_smooth(method = "auto",inherit.aes = FALSE,aes(x=SPEND_EDU,y=mean_all_by_CNT))+
+  ggtitle("Średnia od wydatków na edukacje")+labs(y = "Średni wynik w kraju", x="Wydatki na eduakcje USD/Student")+theme(axis.title.x = element_text(size = 20),
+                                                                                                                                           axis.title.y = element_text(size = 20),
+                                                                                                                                           title = element_text(size=20))+
+  scale_colour_discrete(name = "Kontynenty", labels = c("Ameryki", "Azia", "Europa","Oceania"))+
+  geom_label_repel(aes(label=country.name.en), size=6,color="black" ,data=subset(dane_plot3, SPEND_EDU >16000 ))+
+  geom_label_repel(aes(label=country.name.en), size=6,color="black" ,data=subset(dane_plot3, mean_all_by_CNT >523 ))+
+  geom_label_repel(aes(label=country.name.en), size=6,color="black" ,data=subset(dane_plot3, SPEND_EDU >5000 & mean_all_by_CNT<450))+
+  geom_label_repel(aes(label=country.name.en), size=6,color="black" ,data=subset(dane_plot3, SPEND_EDU <1000 ))+
+  geom_label_repel(aes(label=country.name.en), size=6,color="black" ,data=dane_plot3[25,])
+  
+
+                                                                                                                                       
 
 ggsave("Wykres_SPEND_ON_EDU/all_EDU.png",width = 18, height = 9)
