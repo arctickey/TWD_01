4d5e6b9ac775f64f278a361ff793dfa79ab047ba
Piotr Fic <piotr@localhost.localdomain>
2019-10-30 22:44:25 +0100

    Wydatki na R&D

--- Wykres_spend_RD/skrypt_spend_RD.r
+++ Wykres_spend_RD/skrypt_spend_RD.r
@@ -8,11 +8,11 @@ library(countrycode)
 #dane <- haven::read_sas("~/Programowanie/TWD_01/cy6_ms_cmb_stu_qqq.sas7bdat")
 
 #Wczytanie danych o współczynniku liczba uczniów na nauczyciela (pochodzenie danych OECD)
-ratio_data <- read.csv("Wykres_student_teacher_ratio/Students_per_teacher.csv")
+rd_data <- read.csv("Wykres_spend_RD/Spend_on_RD.csv")
 
 #Usrednienie wartosci wspolczynnika dla krajow z kilku lat badan
-ratio <- group_by(ratio_data, LOCATION) %>%
-  summarise(., avg_ratio = mean(Value, na.rm = TRUE))
+rd_data <- group_by(rd_data, LOCATION) %>%
+  summarise(., avg_rd = mean(Value, na.rm = TRUE))
 
 #Wczytanie etykiet krajow
 codes <- countrycode::codelist %>% select(., c(2, 5, 36))
@@ -22,14 +22,13 @@ result <- pisa2015.mean.pv(c("MATH", "SCIE", "READ"), by = "CNT", data = dane)
 result <- select(result, CNT, Mean)
 
 #Polaczenie ze wspolczynnikiem i dopisanie pelnych nazw panstw
-final <- inner_join(result, ratio, by = c("CNT" = "LOCATION")) %>% left_join(., y = codes, by = c("CNT" = "iso3c"))
+final <- inner_join(result, rd_data, by = c("CNT" = "LOCATION")) %>% left_join(., y = codes, by = c("CNT" = "iso3c"))
 
-#Wykres
-ggplot(final, aes(x = avg_ratio, y=Mean, colour=continent, label = country.name.en))+
+ggplot(final, aes(x = avg_rd, y=Mean, colour=continent, label = country.name.en))+
   geom_point(size=4)+
-  stat_smooth(method = "auto",inherit.aes = FALSE,aes(x=avg_ratio,y=Mean))+
-  ggtitle("Średni wynik względem liczby uczniów przypadających na nauczyciela")+
-  labs(y = "Średni wynik w kraju", x="Liczba uczniów / liczba nauczycieli",color="Kontynent")+
+  stat_smooth(method = "loess",inherit.aes = FALSE,aes(x=avg_rd,y=Mean))+
+  ggtitle("Średni wynik względem wydatków na Reaserch & Development")+
+  labs(y = "Średni wynik w kraju", x="% PKB wydawany na R&D",color="Kontynent")+
   theme(axis.title.x = element_text(size = 14), axis.title.y = element_text(size = 14))
 
-#ggsave("Wykres_student_teacher_ratio/all_student_teacher_ratio_auto.png",width = 18, height = 9)
+#ggsave("Wykres_spend_RD/all_student_spend_RD_auto.png",width = 18, height = 9)
