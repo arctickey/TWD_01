44d777d5fa03a528d3ec118d9dca5073bca8a9d3
arctickey <filipchrzuszcz1@gmail.com>
2019-11-18 14:59:48 +0100

    Wykresy

--- Srednie wyniki/Wykres.R
+++ Srednie wyniki/Wykres.R
@@ -3,6 +3,10 @@ dane[9,1] <- "Smartfon"
 colnames(dane) <- c("Pytanie","Rozwinięte_posiada","Rozwinięte_nie_posiada","Nierozwinięte_posiada"
                     ,"Nierozwinięte_nie_posiada")
 names <- c("Nie posiada","Posiada","Nierozwinięte","Rozwinięte")
+names1 <- c("Słownik","Smartfon","Ciche miejsce do nauki","Łazienka","Biurko",
+                   "Własny pokój","Połączenie z Internetem","Komputer",
+                   "Auto","Dzieło sztuki","Instrument muzyczny","Tablet","Oprogramowanie edukacyjne")
+dane <- dane[order(names1),]
 df2 = tidyr::gather(dane,group,value,-Pytanie)
 gg <- ggplot(data = dane) + theme_minimal()+
   geom_point(data = df2, aes(x = value, color = group,y=Pytanie), size = 5,inherit.aes = FALSE)+
@@ -23,15 +27,16 @@ gg <- ggplot(data = dane) + theme_minimal()+
         panel.border=element_blank(),
         legend.text = element_text(size=10),
         axis.text.y = element_text(size=12),
-
         axis.title = element_text(size=18),
         legend.position = 'top',
         axis.text.x  = element_text(size=12))+
+    scale_y_discrete(limits=rev(names1))+
     scale_color_manual(name = "",labels=names,values = c("#ffd92f","#1f78b4",'#fc8d62',"#66c2a5"))
 plot(gg)
 
 
 ggsave("./PLAKAT/Wyniki w zaleznosci od posiadania itemkow.svg",gg,height=10,width=10)
 ggsave("./Wyniki w zaleznosci od posiadania itemkow.svg",gg,height=10,width=10)
+
 
 
