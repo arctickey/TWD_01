2b37dc89da5349e517631d230a133d9374ec1442
arctickey <filipchrzuszcz1@gmail.com>
2019-11-18 01:06:15 +0100

    Wykresy

--- Srednie wyniki/Wykres.R
+++ Srednie wyniki/Wykres.R
@@ -1,21 +1,33 @@
-
-gg <- ggplot() + theme_minimal()+
-  geom_dumbbell(data=daneWr, aes(x=Wynik_nie_ma_Wr , xend=Wynik_ma_Wr , y=Pytanie, group=Pytanie),
-                color="#1f78b4", size=2,colour_x = '#33a02c',colour_xend = "#e31a1c")+ 
-  geom_dumbbell(data=daneSr, aes(x=Wynik_nie_ma_Wr , xend=Wynik_ma_Wr , y=Pytanie, group=Pytanie),
-                                 color="#ff7f00", size=2,colour_x = '#ffff33',colour_xend = "#a65628")+
+dane <- cbind(daneWr,daneSr)
+colnames(dane) <- c("Pytanie","Rozwinięte_posiada","Rozwinięte_nie_posiada","Nierozwinięte_posiada"
+                    ,"Nierozwinięte_nie_posiada")
+df2 = tidyr::gather(dane,group,value,-Pytanie)
+gg <- ggplot(data = dane) + theme_minimal()+
+  geom_point(data = df2, aes(x = value, color = group,y=Pytanie), size = 4)+
+  geom_dumbbell( aes(x=Rozwinięte_nie_posiada , xend=Rozwinięte_posiada, y=Pytanie, group=Pytanie),
+                color="#e3e2e1", size=2,colour_x = '#33a02c',colour_xend = "#fc8d62")+ 
+  geom_dumbbell( aes(x=Nierozwinięte_posiada , xend=Nierozwinięte_nie_posiada , y=Pytanie, group=Pytanie),
+                                 color="#e3e2e1", size=2,colour_x = "#1f78b4",colour_xend = "#ffd92f")+
   labs(x=NULL, 
        y=NULL, 
-       title="Wynik w zależności od grupy i posiadania róznych itemków") +
-  theme(plot.title = element_text(hjust=0.5, face="bold"),
+       title="Wynik w zależności od grupy i posiadania różnych udogodnień") +
+  theme(plot.title = element_text(hjust=0.5, face="bold",size=18),
         plot.background=element_rect(fill="#f7f7f7"),
         panel.background=element_rect(fill="#f7f7f7"),
         panel.grid.minor=element_blank(),
         panel.grid.major.y=element_blank(),
         panel.grid.major.x=element_line(),
         axis.ticks=element_blank(),
-        panel.border=element_blank())
+        panel.border=element_blank(),
+        legend.text = element_text(size=10),
+        axis.text.y = element_text(size=12),
+        axis.title = element_text(size=18),
+        axis.text.x  = element_text(size=12))+
+    scale_color_manual(name = "", values = c("#ffd92f","#1f78b4",'#33a02c',"#fc8d62"))
 plot(gg)
 
-ggsave("Wyniki w zaleznosci od posiadania itemkow.svg",gg,height=8,width=18)
+
+ggsave("./PLAKAT/Wyniki w zaleznosci od posiadania itemkow.svg",gg,height=8,width=18)
+ggsave("./Wyniki w zaleznosci od posiadania itemkow.svg",gg,height=8,width=18)
+
 
