5ffa7aa3cd296fe56119c5678a781618e26ebf80
arctickey <filipchrzuszcz1@gmail.com>
2019-11-18 14:38:54 +0100

    Wykresy

--- Srednie wyniki/Wykres.R
+++ Srednie wyniki/Wykres.R
@@ -5,12 +5,12 @@ colnames(dane) <- c("Pytanie","Rozwinięte_posiada","Rozwinięte_nie_posiada","N
 names <- c("Nie posiada","Posiada","Nierozwinięte","Rozwinięte")
 df2 = tidyr::gather(dane,group,value,-Pytanie)
 gg <- ggplot(data = dane) + theme_minimal()+
-  geom_point(data = df2, aes(x = value, color = group,y=Pytanie), size = 4)+
+  geom_point(data = df2, aes(x = value, color = group,y=Pytanie), size = 5,inherit.aes = FALSE)+
   geom_dumbbell( aes(x=Rozwinięte_nie_posiada , xend=Rozwinięte_posiada, y=Pytanie, group=Pytanie),
                 color="#66c2a5", size=2,colour_x = '#ffd92f',colour_xend = "#1f78b4")+ 
   geom_dumbbell( aes(x=Nierozwinięte_posiada , xend=Nierozwinięte_nie_posiada , y=Pytanie, group=Pytanie),
                                  color="#fc8d62", size=2,colour_x = "#1f78b4",colour_xend = "#ffd92f")+
-  labs(x=NULL, 
+  labs(x="Średni wynik", 
        y=NULL, 
        title="Wynik w zależności od grupy i posiadania różnych udogodnień") +
   theme(plot.title = element_text(hjust=0.5, face="bold",size=18),
@@ -23,6 +23,7 @@ gg <- ggplot(data = dane) + theme_minimal()+
         panel.border=element_blank(),
         legend.text = element_text(size=10),
         axis.text.y = element_text(size=12),
+
         axis.title = element_text(size=18),
         legend.position = 'top',
         axis.text.x  = element_text(size=12))+
@@ -30,7 +31,7 @@ gg <- ggplot(data = dane) + theme_minimal()+
 plot(gg)
 
 
-ggsave("./PLAKAT/Wyniki w zaleznosci od posiadania itemkow.svg",gg,height=8,width=18)
-ggsave("./Wyniki w zaleznosci od posiadania itemkow.svg",gg,height=8,width=18)
+ggsave("./PLAKAT/Wyniki w zaleznosci od posiadania itemkow.svg",gg,height=10,width=10)
+ggsave("./Wyniki w zaleznosci od posiadania itemkow.svg",gg,height=10,width=10)
 
 
